a70fc2897bd09b11615cc4368997d4b4902e48ac
vaibhav <it@vertipridesolution.com>
2020-05-20 12:28:29 +0530

    Updates

---
+++ precache-manifest.aa2508e08536db36b52fffac921c1b9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5560a4699035a0d5bfc038497b24e8ec",
+    "url": "/reactapp1/index.html"
+  },
+  {
+    "revision": "b735c168aa2bd5ccf21a",
+    "url": "/reactapp1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81b2fe5741e18e18badb",
+    "url": "/reactapp1/static/js/2.06772d1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp1/static/js/2.06772d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b735c168aa2bd5ccf21a",
+    "url": "/reactapp1/static/js/main.cb1af5e6.chunk.js"
+  },
+  {
+    "revision": "2c922a1cd92a69556957",
+    "url": "/reactapp1/static/js/runtime-main.a3f97a2b.js"
+  },
+  {
+    "revision": "23df3266127fded3d5134d4defe73624",
+    "url": "/reactapp1/static/media/img2.23df3266.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp1/static/media/logo.5d5d9eef.svg"
+  }
+]);
